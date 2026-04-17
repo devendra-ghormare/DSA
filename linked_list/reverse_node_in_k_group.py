e328b242ce843eaf710f45e9e8efcce111046f78
Devendra Ghormare <devendraghormare4192@gmail.com>
2026-04-17 23:34:52 +0530

    LC-25: Reverse node in k groups

---
+++ linked_list/reverse_node_in_k_group.py
@@ -0,0 +1,81 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+def arrayToLL(arr):
+    if not arr:
+        return None
+    
+    head = Node(arr[0])
+    curr = head
+
+    for i in range(1, len(arr)):
+        temp = Node(arr[i])
+        curr.next = temp
+        curr = curr.next
+
+    return head
+
+def printLL(head):
+    if not head:
+        return None
+    
+    temp = head
+
+    while temp:
+        print(temp.data, end=" -> ")
+        temp = temp.next 
+    
+    print("None")
+def reverseLL(head):
+    temp = head
+    prev = None
+
+    while temp:
+        curr = temp.next 
+        temp.next = prev
+        prev = temp
+        temp = curr
+    
+    return prev
+
+def getKthNode(head, k):
+    k -= 1
+    temp = head 
+    while temp and k > 0:
+        k -= 1
+        temp = temp.next
+
+    return temp
+ 
+def reverseKGroup(head, k):
+    temp = head
+    prevNode = None
+
+    while temp:
+        kThNode = getKthNode(temp, k)
+
+        if kThNode is None:
+            if prevNode:
+                prevNode.next = temp
+
+            break
+
+        nextNode = kThNode.next 
+        kThNode.next = None
+        newHead = reverseLL(temp)
+
+        if temp == head:
+            head = newHead
+        else:
+            prevNode.next = newHead
+        
+        prevNode = temp
+        temp = nextNode
+    
+    return head
+
+
+head = arrayToLL([1,2,3,4,5])
+printLL(reverseKGroup(head, 2))
