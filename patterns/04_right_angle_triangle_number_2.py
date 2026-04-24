1d3715dad565e80865c72b1e6941899344acdcfb
Devendra Ghormare <devendraghormare4192@gmail.com>
2026-04-24 15:08:04 +0530

    Star right angle tringle number-II pattern

--- patterns/04_right_angle_triangle_number_2.py
+++ patterns/04_right_angle_triangle_number_2.py
@@ -1,9 +1,12 @@
 def right_angle_triangle_number_2(n):
 
     for i in range(1, n+1):
-        for j in range(1, i+1):
+        for _ in range(i):
             print(i, end=" ")
            
         print()
+    
+    # Time = O(n²)
+    # Space = O(1)
 
 right_angle_triangle_number_2(5)
