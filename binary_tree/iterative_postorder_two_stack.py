f3e44d26727830b793e4870f532a0119aff58f86
Devendra Ghormare <devendraghormare4192@gmail.com>
2026-06-15 22:19:15 +0530

    LC-145: Binary tree interative postorder traversal with two stack

---
+++ binary_tree/iterative_postorder_two_stack.py
@@ -0,0 +1,42 @@
+class Node:
+    def __init__(self, val):
+        self.val = val 
+        self.left = None
+        self.right = None
+
+def iterativePostorder(root):
+    postorder = []
+    if not root:
+        return postorder
+    
+    st1 = []
+    st2 = []
+    st1.append(root)
+
+    while st1:
+        node = st1.pop()
+        st2.append(node)
+
+        if node.left:
+            st1.append(node.left)
+        
+        if node.right:
+            st1.append(node.right)
+
+    while st2:
+        postorder.append(st2.pop().val)
+
+    return postorder
+
+# Time = O(n)
+# Space = O(H) where H is the height of the binary tree. The space is used by the stack to store nodes during traversal. 
+
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+
+print(iterativePostorder(root))
