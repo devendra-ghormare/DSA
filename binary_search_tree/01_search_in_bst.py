1ce40539efeb068dc976fb468013b56fa5df1d8f
Devendra Ghormare <devendraghormare4192@gmail.com>
2026-07-24 21:49:59 +0530

    LC-700: Search in binary search tree

---
+++ binary_search_tree/01_search_in_bst.py
@@ -0,0 +1,30 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+def searchBST(root, target):
+    if not root:
+        return None
+
+    while root and root.val != target:
+        if target > root.val:
+            root = root.right
+        else:
+            root = root.left
+
+    return root
+
+# Time = O(h)
+# Space = O(1)
+
+root = Node(4)
+root.left = Node(2)
+root.right = Node(7)
+root.left.left = Node(1)
+root.left.right = Node(3)
+
+
+print(searchBST(root, 8))
+print(searchBST(root, 1))
